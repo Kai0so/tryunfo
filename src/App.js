e19fe1c3f6c4cd912c71df4359be00eff1c6c929
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2022-01-10 15:22:39 -0300

    Lint requisito 4

--- src/App.js
+++ src/App.js
@@ -4,31 +4,41 @@ import Form from './components/Form';
 
 class App extends React.Component {
   constructor() {
-    super()
-    this.state = {}
+    super();
+    this.state = {};
   }
 
   handleInputChange = ({ target: { id, value, checked } }) => {
-    this.setState(id !== 'trunfo' ? { [id]: value  } : { [id]: checked });
+    this.setState(id !== 'trunfo' ? { [id]: value } : { [id]: checked });
   }
 
   render() {
-    const { name, description, attr1, attr2, attr3, imageurl, rarity, trunfo } = this.state;
+    const {
+      name,
+      description,
+      attr1,
+      attr2,
+      attr3,
+      imageurl,
+      rarity,
+      trunfo,
+    } = this.state;
+
     return (
       <>
         <div>
           <h1>Tryunfo</h1>
         </div>
-        <Form onInputChange={this.handleInputChange} />
+        <Form onInputChange={ this.handleInputChange } />
         <Card
-          cardName={name}
-          cardDescription={description}
-          cardAttr1={attr1}
-          cardAttr2={attr2}
-          cardAttr3={attr3}
-          cardImage={imageurl}
-          cardRare={rarity}
-          cardTrunfo={trunfo}
+          cardName={ name }
+          cardDescription={ description }
+          cardAttr1={ attr1 }
+          cardAttr2={ attr2 }
+          cardAttr3={ attr3 }
+          cardImage={ imageurl }
+          cardRare={ rarity }
+          cardTrunfo={ trunfo }
         />
       </>
     );
